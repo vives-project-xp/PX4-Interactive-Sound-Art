c575a0364461e0baf5aa548adcbe09af6f824c82
Joren Vandewalle <joren.vandewalle@gmail.com>
2025-04-24 09:06:12 +0200

    update main js

--- interface/frontend/src/main.js
+++ interface/frontend/src/main.js
@@ -2,19 +2,20 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import io from 'socket.io-client' // Import socket.io-client
 
 import App from './app.vue'
 import router from './router' // Import the router
 
+// Establish WebSocket connection
+const socket = io('http://localhost:4000')
 
 const app = createApp(App)
 
-createApp(App).mount('#app')
+// Provide the socket instance globally
+app.config.globalProperties.$socket = socket
 
 app.use(createPinia())
 app.use(router) // Use the router
 
-app.mount('#app')
-
-
-
+app.mount('#app') // Mount the app instance after applying plugins
