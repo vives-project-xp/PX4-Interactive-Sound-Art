7c7d955497216573eba3450d20e83a7e2086dd6b
Joren Vandewalle <joren.vandewalle@gmail.com>
2025-04-24 10:22:54 +0200

    update main

--- interface/frontend/src/main.js
+++ interface/frontend/src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import App from './App.vue';
+
 import router from './router';
 import socketService from './services/socketService.js';
 
